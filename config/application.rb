ad5f100c57168ff097e69c56bd259e0e44373932
Konstantin Goryachev <kevinglare@gmail.com>
2022-06-15 10:51:21 +0300

    fix: adding X-Frame-Options

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module SecurityTestbed
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.action_dispatch.default_headers['X-Frame-Options'] = 'DENY'
 
     # Configuration for the application, engines, and railties goes here.
     #
